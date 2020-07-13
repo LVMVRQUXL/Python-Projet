3bee86566e33ad07b7ed436ac00908d6be2ecee4
Loïc Lamarque <loiclamarque777@gmail.com>
2020-07-13 19:12:27 +0200

    feat: Implement account's removing

--- account.py
+++ account.py
@@ -6,35 +6,37 @@ def is_valid_str(string: str) -> bool:
 
 class Account:
     def __init__(self, username: str, password: str, description: str):
-        if (is_valid_str(username) or is_valid_str(password) or is_valid_str(description)) is False:
+        if is_valid_str(username) is False or \
+                is_valid_str(password) is False or \
+                is_valid_str(description) is False:
             raise ValueError
         self.username = username
         self.password = password
         self.description = description
 
-    def __get__username(self) -> str:
-        return self.__username
+    def __get__description(self) -> str:
+        return self.__description
 
     def __get__password(self) -> str:
         return self.__password
 
-    def __get__description(self) -> str:
-        return self.__description
+    def __get__username(self) -> str:
+        return self.__username
 
-    def __set__username(self, username: str):
-        self.__username = username
+    def __set__description(self, description: str):
+        self.__description = description
 
     def __set__password(self, password: str):
         self.__password = password
 
-    def __set__description(self, description: str):
-        self.__description = description
+    def __set__username(self, username: str):
+        self.__username = username
 
     def __str__(self) -> str:
         return 'Account(username: {}, password: {}, description: {})'.format(
             self.username, self.password, self.description
         )
 
-    username = property(__get__username, __set__username)
-    password = property(__get__password, __set__password)
     description = property(__get__description, __set__description)
+    password = property(__get__password, __set__password)
+    username = property(__get__username, __set__username)
--- main.py
+++ main.py
@@ -7,7 +7,16 @@ def main():
     account_manager = AccountManager()
     account_manager.add_account(acc)
     accounts = account_manager.get_all_accounts()
-    [print(acc) for acc in accounts]
+    if len(accounts) == 0:
+        print('No accounts to print!')
+    else:
+        [print(acc) for acc in accounts]
+    account_manager.remove_account(acc)
+    accounts = account_manager.get_all_accounts()
+    if len(accounts) == 0:
+        print('No accounts to print!')
+    else:
+        [print(acc) for acc in accounts]
 
 
 if __name__ == '__main__':
