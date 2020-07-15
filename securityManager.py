4404fb4faa4a93f493caa5df1a54720afa3a4a9b
Loïc Lamarque <loiclamarque777@gmail.com>
2020-07-15 22:42:28 +0200

    refactor: Refactor securityManager.py

--- securityManager.py
+++ securityManager.py
@@ -9,24 +9,29 @@ class SecurityManager:
             self.__create_key_security_file()
         else:
             self.__read_secret_key_from_file()
+        self.__init_cipher()
 
     def encrypt_password(self, password: str) -> str:
-        cipher = Fernet(self.__secret_key)
-        return cipher.encrypt(password.encode()).decode()
+        return self.__cipher.encrypt(password.encode()).decode()
 
     def decrypt_password(self, password: str) -> str:
-        cipher = Fernet(self.__secret_key)
-        return cipher.decrypt(password.encode()).decode()
+        return self.__cipher.decrypt(password.encode()).decode()
 
     def __create_key_security_file(self):
-        self.__secret_key = Fernet.generate_key()
+        self.__init_secret_key(Fernet.generate_key())
         with open('key.security', 'w') as file:
             file.write('{}\n'.format(self.__secret_key.decode()))
+
+    def __init_cipher(self):
+        self.__cipher = Fernet(self.__secret_key)
+
+    def __init_secret_key(self, value: bytes):
+        self.__secret_key = value
 
     def __read_secret_key_from_file(self):
         with open('key.security', 'r') as file:
             key = ''.join(file.read().splitlines())
-            self.__secret_key = key.encode()
+            self.__init_secret_key(key.encode())
 
     def __str__(self) -> str:
         return 'SecurityManager(secret_key: {})'.format(self.__secret_key)
