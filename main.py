1819c64ea79719d921193ca8fe0ec0a53df9b16d
AkioWm <cherif.kante16@gmail.com>
2020-07-17 23:28:24 +0200

    refactor: remove useless function

--- main.py
+++ main.py
@@ -4,34 +4,6 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 from controller import MainWindow
 
 import sys
-def main():
-    acc = Account('test', 'ozef', 'No website')
-    account_manager = AccountManager()
-    account_manager.add_account(acc)
-    accounts = account_manager.get_all_accounts()
-    if len(accounts) == 0:
-        print('No accounts to print!\n')
-    else:
-        [print(acc) for acc in accounts]
-        print('\n')
-
-    new_acc = Account('test', 'password_updated', 'No description')
-    account_manager.update_account(acc, new_acc)
-    accounts = account_manager.get_all_accounts()
-    if len(accounts) == 0:
-        print('No accounts to print!\n')
-    else:
-        [print(acc) for acc in accounts]
-        print('\n')
-
-    account_manager.remove_account(acc)
-    accounts = account_manager.get_all_accounts()
-    if len(accounts) == 0:
-        print('No accounts to print!\n')
-    else:
-        [print(acc) for acc in accounts]
-        print('\n')
-
 
 if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
